aafb24b0785d250983cd1ce688a537d88802841f
Daniela <danielacardona2203@gmail.com>
2024-01-07 12:06:07 +0100

    Add counter update function

--- script.js
+++ script.js
@@ -2,4 +2,16 @@ const counters = document.querySelectorAll(".counter");
 
 counters.forEach((counter) => {
     counter.innerText = 0;
+
+    function updateCounter() {
+        const target = +counter.dataset.target;
+        const count = +counter.innerText;
+        const increment = target / 200;
+
+        if (count < target) {
+            counter.innerText = `${Math.ceil(count + increment)
+                }`;
+            setTimeout(update, 10);
+        }
+    }
 })
