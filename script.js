7553d082f5b36efd9bde368fd9f1d0d472189d4e
Daniela <danielacardona2203@gmail.com>
2024-01-07 12:31:17 +0100

    implement scroll-triggered counter update

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
-const counters = document.querySelectorAll(".counter");
+const counters = document.querySelectorAll('.counter');
 
 counters.forEach((counter) => {
-    counter.innerText = 0;
+  counter.innerText = 0;
 
-    function updateCounter() {
-        const target = +counter.dataset.target;
-        const count = +counter.innerText;
-        const increment = target / 200;
+  function updateCounter() {
+    const target = +counter.dataset.target;
+    const count = +counter.innerText;
+    const increment = target / 200;
 
-        if (count < target) {
-            counter.innerText = `${Math.ceil(count + increment)
-                }`;
-            setTimeout(update, 10);
-        } else {
-            counter.innerText = target; 
-        }
-    } 
+    if (count < target) {
+      counter.innerText = `${Math.ceil(count + increment)}`;
+      setTimeout(updateCounter, 10);
+    } else {
+      counter.innerText = target;
+    }
+  }
 
-    // updateCounter();
-    window.addEventListener("scroll", () => {
-        const scrollHeight = window.pageYOffset; 
-        const sectionTop = document.querySelector(".top");
-        const sectionTopHeight = sectionTop.clientHeight;
-        console.log(sectionTopHeight)
+  // updateCounter();
+  window.addEventListener('scroll', () => {
+    const scrollHeight = window.scrollY;
+    const sectionTop = document.querySelector('.top');
+    const sectionTopHeight = sectionTop.clientHeight;
+    console.log(sectionTopHeight);
 
-    })
-})
+    if (scrollHeight >= sectionTopHeight - 1) {
+      updateCounter();
+    }
+  });
+});
